a8a8671e6014e13c4a5631e5e0cf989d4cf3121e
Doshvargard <[doshvargard@gmail.com]>
2021-08-19 23:57:22 +0430

    Hey console

--- js/controller.js
+++ js/controller.js
@@ -4,7 +4,7 @@ import view from "./view";
 import * as model from "./model";
 
 const controlCurrentPlayer = function (curPlayer) {
-  console.log("Hi");
+  console.log("Hey!");
   if (model.state.currentPlayer) return;
   model.setCurrentPlayer(curPlayer);
   view.rander(model.state);
